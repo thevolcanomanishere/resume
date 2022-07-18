6bdc9ade0b147442e905f0241ad2a589bb2c48dd
Alex McG <alex.mcgonagle.dev@gmail.com>
2022-07-18 15:40:46 +0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,272 @@
+import { RiUserLocationFill, RiGithubFill } from "react-icons/ri";
+import ProfileImage from "./assets/alex512.jpeg";
+
+function App() {
+  return (
+    <body className="flex-row w-4/5 max-w-[1000px] m-auto p-2">
+      <header className="flex m-auto justify-between mb-5">
+        <section className="flex">
+          <div className="h-[150px] w-[150px] mr-5 rounded-md">
+            <img src={ProfileImage} />
+          </div>
+          <div className="flex-row">
+            <h1 className="font-bold text-4xl">Alex McGonagle</h1>
+            <h2 className="font-light text-xl">Software Engineer</h2>
+          </div>
+        </section>
+
+        <section className="flex">
+          <div className="border mr-3"></div>
+          <div>
+            <h2 className="text-xl">About</h2>
+            <hr />
+            <div className="flex items-center">
+              <RiUserLocationFill className="mr-2" />
+              <p>Dublin, Ireland</p>
+            </div>
+            <div className="flex items-center">
+              <RiGithubFill className="mr-2" />
+              <a
+                className="text-blue-500"
+                href="https://github.com/thevolcanomanishere"
+                target="_blank"
+              >
+                github.com/thevolcanomanishere
+              </a>
+            </div>
+          </div>
+        </section>
+      </header>
+
+      <div className="flex-row">
+        <section>
+          <div className="flex-row w-full">
+            <div className="flex-row items-center">
+              <h2 className="text-xl mr-2 font-serif">Skills</h2>
+              <div className="flex-grow h-px bg-gray-400 mb-2"></div>
+            </div>
+            <div className="flex-row space-y-2 mb-2">
+              <p>Typescript, React, GraphQL, Ethers, Next.js, Tailwind, Jest</p>
+              <p>
+                Node.js, Express, Fastify, MongoDB, Google App Engine, Cloud
+                Functions, Firebase, Web Sockets, Memcached, Redis, AWS Lambdas,
+                Amazon S3, Google Cloud Storage, Google Cloud Run, Serverless
+                Framework, Cloudflare Workers, Cloudflare R3, IPFS, Pinata,
+                Fleek, Storybook
+              </p>
+              <p>Docker, Postman, Twilio, Git</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="flex-row w-full">
+            <div className="flex-row items-center">
+              <h2 className="text-xl mr-2 font-serif">
+                Professional Experience
+              </h2>
+              <div className="flex-grow h-px bg-gray-400 mb-2"></div>
+            </div>
+            <div className="flex-row space-y-2 mb-2">
+              <div className="flex justify-between">
+                <h2 className="font-bold text-lg">
+                  Software Engineer & Co-Founder
+                </h2>
+                <p>2022 Jan - Present</p>
+              </div>
+              <h2>
+                <a
+                  href="https://github.com/mello-defi"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  Mello DeFi - Open Source
+                </a>
+              </h2>
+              <p>
+                Mello DeFi aims to educate new users to DeFi by guiding them
+                through the most common DeFi actions and streamlining the
+                onboarding experience. Mello has not fully launched due to
+                market conditions.
+              </p>
+              <ul className="ml-4 list-disc">
+                <li>Successfully awarded a grant from Polygon and Aave</li>
+                <li>
+                  Worked directly with UI/UX contractors on frontend overhaul
+                </li>
+                <li>Made small contributions to the codebase</li>
+              </ul>
+            </div>
+
+            <div className="flex-row space-y-2 mb-2">
+              <div className="flex justify-between">
+                <h2 className="font-bold text-lg">Consultant & Co-Founder</h2>
+                <p>2021 May - Present</p>
+              </div>
+              <h2>
+                <a
+                  href="https://github.com/mello-defi"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  The DeFi Coach
+                </a>
+              </h2>
+              <p>
+                The DeFi Coach is an online education course for people wanting
+                to learn about and use decentralised finance safely.
+              </p>
+              <ul className="ml-4 list-disc">
+                <li>
+                  Created 8+ hours of educational video content across a wide
+                  range of DeFi subjects
+                </li>
+                <li>
+                  Researched the latest DeFi applications and their place in the
+                  ecosystem
+                </li>
+                <li>
+                  Helped students with questions they have on the content in the
+                  course
+                </li>
+                <li>
+                  Consulted with businesses and private individuals on
+                  decentralised finance
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex-row space-y-2 mb-2">
+              <div className="flex justify-between">
+                <h2 className="font-bold text-lg">
+                  Software Engineer & Co-Founder
+                </h2>
+                <p>2019 Mar - 2021 Dec</p>
+              </div>
+              <h2>Bottleneck</h2>
+              <p>
+                Bottleneck creates machine learning powered chatbots to give
+                instant answers to the top questions that your business or
+                organisation receives.
+              </p>
+              <ul className="ml-4 list-disc">
+                <li>
+                  Extended the functionality of the Manychat.com bot platform
+                  via Google Cloud Functions / App Engine
+                </li>
+                <li>
+                  Built API connections between the Manychat platform to Twilio,
+                  Slack, MongoDB, Google Sheets, and Dialogflow
+                </li>
+                <li>
+                  Deployed Node.js/Express, Node.js/Fastify servers using Google
+                  App Engine, Cloud Run, AWS Lambda
+                </li>
+                <li>
+                  Designed and implemented low-latency, high-availability, and
+                  performant applications
+                </li>
+                <li>
+                  Created charts for customer dashboards using MongoDB Charts
+                  system
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex-row space-y-2 mb-2">
+              <div className="flex justify-between">
+                <h2 className="font-bold text-lg">Lead Frontend Engineer</h2>
+                <p>2017 Jun - 2019 Mar</p>
+              </div>
+              <h2>
+                <a
+                  href="https://cartrawler.com"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  Cartrawler
+                </a>
+              </h2>
+              <ul className="ml-4 list-disc">
+                <li>
+                  Designed, built, and maintained production React-Native apps
+                </li>
+                <li>
+                  Created performant and maintainable code using Javascript +
+                  Flow, Java, and Objective-C
+                </li>
+                <li>Created native bridge modules for iOS and Android</li>
+                <li>
+                  Created bash scripts to automate deployments to CodePush and
+                  create source maps for Sentry
+                </li>
+                <li>
+                  Directly managed a team of software engineers daily using an
+                  agile methodology
+                </li>
+                <li>Managed and track tasks using Jira</li>
+                <li>Code review pulls requests using Stash/Jira integration</li>
+                <li>
+                  Developed, updated, and advocated for the technical direction
+                  of the mobile product
+                </li>
+                <li>
+                  Acted as an interface between the engineers, business
+                  analysts, and partner managers
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex-row space-y-2 mb-2">
+              <div className="flex justify-between">
+                <h2 className="font-bold text-lg">
+                  Graduate Software Engineer
+                </h2>
+                <p>2016 Sep - 2017 Jun</p>
+              </div>
+              <h2>
+                <a
+                  href="https://cartrawler.com"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  Cartrawler
+                </a>
+              </h2>
+              <ul className="ml-4 list-disc">
+                <li>
+                  Designed, built, and maintained production React-Native apps
+                </li>
+                <li>
+                  Created performant and maintainable code using Javascript +
+                  Flow, Java, and Objective-C
+                </li>
+                <li>Created native bridge modules for iOS and Android</li>
+                <li>
+                  Created bash scripts to automate deployments to CodePush and
+                  create source maps for Sentry
+                </li>
+                <li>
+                  Directly managed a team of software engineers daily using an
+                  agile methodology
+                </li>
+                <li>Managed and track tasks using Jira</li>
+                <li>Code review pulls requests using Stash/Jira integration</li>
+                <li>
+                  Developed, updated, and advocated for the technical direction
+                  of the mobile product
+                </li>
+                <li>
+                  Acted as an interface between the engineers, business
+                  analysts, and partner managers
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </body>
+  );
+}
+
+export default App;
